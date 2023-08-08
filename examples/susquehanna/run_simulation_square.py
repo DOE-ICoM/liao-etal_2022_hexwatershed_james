a989396ede16340bd7463e701ee2ae557385bda5
changliao1025 <changliao1025@outlook.com>
2023-08-08 14:01:04 -0700

    update figures

--- examples/susquehanna/run_simulation_square.py
+++ examples/susquehanna/run_simulation_square.py
@@ -26,21 +26,21 @@ else:
 #===========================
 #setup case information
 #===========================
-iFlag_create_job=1
-iFlag_visualization =0 
+iFlag_create_job=0
+iFlag_visualization =1
 iCase_index = 5
 aResolution_meter = [5000, 40000]
 nResolution = len(aResolution_meter)
 sMesh_type = 'square'
-sDate='20220901'
+sDate='20230701'
 
 #===================================
 #setup output and HPC job 
 #===================================
 if iFlag_create_job ==1:
     sSlurm = 'short'
-    sFilename = sWorkspace_output + '/' + sMesh_type + '.bash'
-    ofs = open(sFilename, 'w')
+    sFilename_job = sWorkspace_output + '/' + sMesh_type + '.bash'
+    ofs = open(sFilename_job, 'w')
     sLine  = '#!/bin/bash' + '\n'
     ofs.write(sLine)
 
@@ -57,46 +57,54 @@ aExtent_dam = [-75.75,-76.15, 42.1,42.5] #dam
 for iResolution in range(1, nResolution+1):        
     dResolution_meter = aResolution_meter[iResolution-1]
 
-    #===================================
-    #topology turn off
-    #===================================
-    iFlag_stream_burning_topology = 0
-    iFlag_use_mesh_dem = 0
-    iFlag_elevation_profile = 0
-    oPyhexwatershed = pyhexwatershed_read_model_configuration_file(sFilename_configuration_in,\
-        iCase_index_in=iCase_index,\
-            iFlag_stream_burning_topology_in=iFlag_stream_burning_topology,\
-                iFlag_use_mesh_dem_in=iFlag_use_mesh_dem,\
-                    iFlag_elevation_profile_in=iFlag_elevation_profile,\
-             dResolution_meter_in = dResolution_meter, sDate_in= sDate, sMesh_type_in= sMesh_type)   
-    if iFlag_create_job ==1:
-        oPyhexwatershed._create_hpc_job()   
-        sLine  = 'cd ' + oPyhexwatershed.sWorkspace_output + '\n'
-        ofs.write(sLine)
-        sLine  = 'sbatch submit.job' + '\n'
-        ofs.write(sLine)
-    
-    iCase_index = iCase_index + 1
+    for iFlag_stream_burning_topology in [0,1]:
+        iFlag_use_mesh_dem = 0
+        iFlag_elevation_profile = 0
+        oPyhexwatershed = pyhexwatershed_read_model_configuration_file(sFilename_configuration_in,\
+            iCase_index_in=iCase_index,\
+                iFlag_stream_burning_topology_in=iFlag_stream_burning_topology,\
+                    iFlag_use_mesh_dem_in=iFlag_use_mesh_dem,\
+                        iFlag_elevation_profile_in=iFlag_elevation_profile,\
+                 dResolution_meter_in = dResolution_meter, sDate_in= sDate, sMesh_type_in= sMesh_type)   
+        if iFlag_create_job ==1:
+            oPyhexwatershed._create_hpc_job()   
+            sLine  = 'cd ' + oPyhexwatershed.sWorkspace_output + '\n'
+            ofs.write(sLine)
+            sLine  = 'sbatch submit.job' + '\n'
+            ofs.write(sLine)
+        else:
+            #oPyhexwatershed.export() #for testing
+            pass
 
-    #===================================
-    #topology turn on
-    #===================================
-    iFlag_stream_burning_topology = 1
-    oPyhexwatershed = pyhexwatershed_read_model_configuration_file(sFilename_configuration_in,\
-        iCase_index_in=iCase_index,  iFlag_stream_burning_topology_in=iFlag_stream_burning_topology,\
-              iFlag_use_mesh_dem_in=iFlag_use_mesh_dem,\
-                    iFlag_elevation_profile_in=iFlag_elevation_profile,\
-            dResolution_meter_in = dResolution_meter, sDate_in= sDate, sMesh_type_in= sMesh_type)   
-    
-    if iFlag_create_job ==1:
-        oPyhexwatershed._create_hpc_job()   
-        sLine  = 'cd ' + oPyhexwatershed.sWorkspace_output + '\n'
-        ofs.write(sLine)
-        sLine  = 'sbatch submit.job' + '\n'
-        ofs.write(sLine)
+        if iFlag_visualization == 1:
+            pBasin_hexwatershed = oPyhexwatershed.aBasin[0]
+            sWorkspace_output_basin = pBasin_hexwatershed.sWorkspace_output_basin
 
-    iCase_index = iCase_index + 1
+            #polyline
+            sFilename = os.path.join( sWorkspace_output_basin, 'flow_direction.png' )
+            #oPyhexwatershed.plot( sVariable_in = 'flow_direction', sFilename_output_in = sFilename, iFont_size_in= 14)
+
+            #polygon    
+            sFilename = os.path.join(  sWorkspace_output_basin, 'surface_elevation.png' )    
+            #oPyhexwatershed.plot( sVariable_in = 'elevation', sFilename_output_in = sFilename, iFont_size_in= 16, dData_min_in=0, dData_max_in=800, iFlag_colorbar_in = 0)     
+
+            sFilename = os.path.join(  sWorkspace_output_basin, 'surface_slope.png' )        
+            #oPyhexwatershed.plot( sVariable_in = 'slope', sFilename_output_in = sFilename, iFont_size_in= 16, dData_min_in=0, dData_max_in=0.1 , iFlag_colorbar_in = 0)
+
+            sFilename = os.path.join( sWorkspace_output_basin, 'drainage_area.png' )
+            #oPyhexwatershed.plot( sVariable_in = 'drainage_area',  sFilename_output_in = sFilename, iFont_size_in= 16, dData_min_in=0, dData_max_in=1E10, iFlag_colorbar_in=0 )
+
+            sFilename = os.path.join(  sWorkspace_output_basin, 'travel_distance.png' )
+            oPyhexwatershed.plot( sVariable_in = 'travel_distance', sFilename_output_in = sFilename, iFont_size_in= 16, dData_min_in=0, dData_max_in=1E6 ,iFlag_colorbar_in=0)
+            #mixed
+            sFilename = os.path.join( sWorkspace_output_basin, 'flow_direction_w_mesh.png' )
+            #oPyhexwatershed.plot( sVariable_in = 'flow_direction_with_mesh', sFilename_output_in = sFilename)  
+
+            sFilename = os.path.join(  sWorkspace_output_basin, 'flow_direction_w_observation.png' )
+            #oPyhexwatershed.plot( sVariable_in = 'flow_direction_with_observation',  sFilename_output_in = sFilename, iFont_size_in= 16)
+        
+        iCase_index = iCase_index + 1
             
 if iFlag_create_job ==1:
     ofs.close()
-    os.chmod(sFilename, stat.S_IREAD | stat.S_IWRITE | stat.S_IXUSR) 
+    os.chmod(sFilename_job, stat.S_IREAD | stat.S_IWRITE | stat.S_IXUSR) 
